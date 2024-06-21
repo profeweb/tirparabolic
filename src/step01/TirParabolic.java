e36adc8b054ccc69591edb1dc80ccc9cecba93d3
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2024-06-21 10:40:06 +0200

    Initial

---
+++ src/step01/TirParabolic.java
@@ -0,0 +1,25 @@
+package step01;
+
+import processing.core.PApplet;
+import stepFinal.Projectil;
+import stepFinal.Target;
+
+public class TirParabolic extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("step01.TirParabolic");
+    }
+
+    public void settings(){
+        size(1600, 800);
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+        background(220);
+    }
+
+}
